bb0bf08a88891b0bc546e809e7004632f57be52f
Pierluigi Caruso <pierluigic97@gmail.com>
2020-02-05 22:54:47 +0100

    Updates

---
+++ precache-manifest.5e910c54cec6a7b8bc65a6f1724d9640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fe46e33bf95ade3b54b1453b755c36",
+    "url": "/RecipeBox/index.html"
+  },
+  {
+    "revision": "a73ce0d868106c468af0",
+    "url": "/RecipeBox/static/css/main.33e8ba43.chunk.css"
+  },
+  {
+    "revision": "61a335d79ab12ac04aca",
+    "url": "/RecipeBox/static/js/2.021ee069.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RecipeBox/static/js/2.021ee069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73ce0d868106c468af0",
+    "url": "/RecipeBox/static/js/main.b2dc066c.chunk.js"
+  },
+  {
+    "revision": "961004ba969e5da6288f",
+    "url": "/RecipeBox/static/js/runtime-main.d465b831.js"
+  }
+]);
